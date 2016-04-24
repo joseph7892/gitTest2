31475672343a5748b759734139e5e8c2a2ede66a
superman <superman@superman>
2016-04-23 20:50:14 -0400

    Signed-off-by: superman <superman@superman>

---
+++ gitTest/src/com/test/gitTest/Test2.java
@@ -0,0 +1,14 @@
+package com.test.gitTest;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		CoolCat cat1 = new CoolCat();
+		cat1.setName("minmin");
+		cat1.setType("persian");
+
+		System.out.println(cat1.toString());
+
+	}
+
+}
